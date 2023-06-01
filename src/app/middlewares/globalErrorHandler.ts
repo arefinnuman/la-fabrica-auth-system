86cb4713a3016528146db03084efec95cc17ce4f
arefinnuman <arefinnuman2000@gmail.com>
2023-06-02 01:00:40 +0600

    Validation handler added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
 
+import config from '../config'
+import { IGenericErrorMessage } from '../interfaces/IGenericErrorMessage'
+
 const globalErrorHandler = (
-  err: unknown,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({ Error: err })
+  const statusCode = 500
+  const message = 'Something went wrong'
+  const errorMessages: IGenericErrorMessage[] = []
+
+  if (err?.name === 'ValidationError') {
+    // const simplifiedError = handleValidationError(err)
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  })
   next()
 }
 
---
+++ src/errors/handleValidationError.ts
@@ -0,0 +1,10 @@
+// import mongoose from 'mongoose';
+// import { IGenericErrorMessage } from '../app/interfaces/IGenericErrorMessage';
+
+// const handleValidationError = (err: mongoose.Error.ValidationError) => {
+//     const errors: IGenericErrorMessage[] = Object.values(err.errors).map((el: mongoose.Error.ValidationError) => {
+//       return {
+//         path: el?.path,
+//         message: el?.message
+//       };
+//     });
